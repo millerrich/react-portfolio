4cc0dac84e88de9ab69f54393f996bf76d778faf
Miller Rich <william.miller.rich@gmail.com>
2020-10-20 18:46:06 -0500

    update home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Navbar from '../components/navbar/nav';
+import me from '../images/linkedin.jpg';
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <>
             <Navbar />
-        </div>
+        <div className="container">
+            <h1>Home</h1>
+            <div className="row">
+              <div className="col-12" style={{border: 'double 10px black', backgroundColor: 'silver'}}>
+                <img className="float-left" src={me} alt="me" width="35%" height="auto" style={{margin: '20px', border: 'solid 2px black'}} />
+                <div style={{marginTop: "20px"}}>
+                <p>Miller Rich</p>
+                <p>william.miller.rich@gmail.com</p>
+                <p>615-123-4567</p>
+                <p>https://github.com/millerrich</p>
+                <p>resume</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          </>
     )
 }
 
