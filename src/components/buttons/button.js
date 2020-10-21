a7446e97766e83249b42cdeb6b341289771944ba
Miller Rich <william.miller.rich@gmail.com>
2020-10-20 22:08:56 -0500

    update column.js and portfolio.js, render portfolio to portfolio page

---
+++ src/components/buttons/button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+    return(
+        <>
+        <button type="button" className="btn btn-dark"></button>
+        </>
+    )
+}
+
+export default Button;
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from '../components/navbar/nav';
-
+import PortDisplay from '../components/portrows/column';
 
 function Portfolio() {
     return (
         <div>
-            <h1>Portfolio</h1>
             <Navbar />
+            <PortDisplay />
         </div>
     )
 }
